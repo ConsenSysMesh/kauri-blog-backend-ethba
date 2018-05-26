a858ce5c0c4fdc550f5dfbcb71849380dbbddb8e
Kendall Cole <kcole16@gmail.com>
2018-05-26 13:26:20 -0300

    Mint ERC721 token upon blog publishing

--- smart-contracts/contracts/Blog.sol
+++ smart-contracts/contracts/Blog.sol
@@ -1,11 +1,14 @@
 pragma solidity ^0.4.23;
 
-contract Blog {
+import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
+
+contract Blog is ERC721Token {
   event PostSubmitted(bytes32 indexed id, bytes32 ipfsHash, address indexed creator);
   event BlogTipped(address indexed tipper, address indexed author, uint amount);
   event PostTipped(bytes32 indexed id, address indexed tipper, uint amount);
 
   mapping (bytes32 => bool) postExists;
+  uint numberOfPosts = 0;
 
   constructor() public {
     // owner = msg.sender;
@@ -18,6 +21,8 @@ contract Blog {
   function submitPost(bytes32 _id, bytes32 _ipfsHash) {
     require(postExists[_id] == false);
     postExists[_id] = true;
+    numberOfPosts ++;
+    _mint(msg.sender, numberOfPosts);
     emit PostSubmitted(_id, _ipfsHash, msg.sender);
   }
 
