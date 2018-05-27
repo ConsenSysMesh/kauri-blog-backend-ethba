68699a744f3706040ccc308765dbc6d125a4a159
Kendall Cole <kcole16@users.noreply.github.com>
2018-05-27 09:00:52 -0300

    Update truffle.js

--- smart-contracts/truffle.js
+++ smart-contracts/truffle.js
@@ -1,25 +1,11 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
-var mnemonic = "fragile emotion security lion expand world gather chunk fish way summer gravity"
-
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*", // Match any network id
-    },
-    rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/")
-      },
-      network_id: 4
-    },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/")
-      },
-      network_id: 3
-    }    
+    } 
   }
 };
