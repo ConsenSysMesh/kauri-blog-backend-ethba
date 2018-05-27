435b9e6dbf2cf08d875870cd5dc4cd8018906dc0
Craig <craigwilliams84@googlemail.com>
2018-05-27 01:19:42 -0300

    Added json type annotations

--- src/main/java/io/kauri/dbt/message/Message.java
+++ src/main/java/io/kauri/dbt/message/Message.java
@@ -1,7 +1,17 @@
 package io.kauri.dbt.message;
 
-public interface Message<D> {
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "type",
+        visible = true)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = EventeumMessage.class, name = EventeumMessage.TYPE),
+})
+public interface Message<D> {
     String getType();
 
     D getDetails();
